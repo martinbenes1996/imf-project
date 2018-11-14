91c837001b4fd2686049f0603e42c527c592020c
Martin Benes <xbenes49@stud.fit.vutbr.cz>
2018-11-14 21:34:48 +0100

    plotting

--- plot.py
+++ plot.py
@@ -2,44 +2,65 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 
+Xdata, Ydata = np.meshgrid(np.linspace(0, 1, 50, endpoint=True), np.linspace(0, 1, 50, endpoint=True))
+
 class Negator:
     @staticmethod
     def standard(x):
         return 1-x
-    def godel(x):
-        return np.array( [1 if i == 0 else 0 for i in x] )
+    @classmethod
+    def godel(cls, x):
+        if isinstance(x, float) or isinstance(x, int):
+            if x == 0: return 1
+            else: return 0
+        l = []
+        for i in x:
+            l.append( cls.godel(i) )
+        return np.array(l)
 
 class Implicator:
     @staticmethod
-    def maximum(x, y, negator=Negator.standard):
+    def maximum(x,y, negator=Negator.standard):
         return np.maximum(negator(x), y)
     @staticmethod
     def probsum(x,y, negator=Negator.standard):
         return negator(x) + y - negator(x)*y
+    @staticmethod
+    def drastic(x,y, negator=Negator.standard):
+        l = []
+        for i,j in zip(negator(x),y):
+            k = []
+            for m,n in zip(i,j):
+                if min(m,n) == 0: k.append(max(m,n))
+                else: k.append(1)
+            l.append(np.array(k))
+        return np.array(l)
+    @staticmethod
+    def reichenbach(x,y, negator=Negator.standard):
+        return np.minimum(negator(x)+y, 1)
+    @staticmethod
+    def lukasiewicz(x,y, negator=Negator.standard):
+        l = []
+        for i,j in zip(negator(x),y):
+            k = []
+            for m,n in zip(i,j):
+                if n==0: k.append( m )
+                elif m==1: k.append(  )
+                if min(m,n) == 0: k.append(max(m,n))
+                else: k.append(1)
+            l.append(np.array(k))
+        return np.array(l)
+        return np.minimum(negator(x)+y, 1)
+        
 
-def plotMaximum(x,y, negator=Negator.standard):
+def plotImplication(implicator=Implicator.maximum, negator=Negator.standard):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    z = Implicator.maximum(X, Y, negator)
-    ax.contour3D(y, x, z, 70, cmap='plasma')
-    ax.set_xlabel('y')
-    ax.set_ylabel('x')
+    Zdata = implicator(Xdata, Ydata, negator)
+    #ax.contour3D(Ydata, Xdata, Zdata, 40, cmap='plasma')
+    ax.plot_surface(Xdata, Ydata, Zdata, cmap='plasma')
+    plt.axis([Xdata.max(), Xdata.min(), Ydata.min(), Ydata.max()])
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
     ax.set_zlabel('z')
     fig.show()
-
-def plotProbsum(x,y):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    z = Implicator.probsum(X,Y)
-    ax.contour3D(y, x, z, 70, cmap='plasma')
-    ax.set_xlabel('y')
-    ax.set_ylabel('x')
-    ax.set_zlabel('z')
-    fig.show()
-
-if __name__=="__main__":
-    x = np.linspace(0, 1, 1000)
-    y = np.linspace(0, 1, 1000)
-    X, Y = np.meshgrid(x, y)
-    
-    plotMaximum(X,Y)
